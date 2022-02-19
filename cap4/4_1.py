cecb26924482618b1d2db7dfddbb72b8f5733327
auvmx <angelurielvelascom@gmail.com>
2022-02-18 18:46:48 -0600

    cambio de nombre de carpetas

--- cap4/4_1.py
+++
@@ -1,5 +0,0 @@
-favorite_pizzas = ["Peperoni", "Hawaiana", "Mexicana"]
-for type_pizza in favorite_pizzas:
-    print(f"I like {type_pizza} pizza.")
-
-print(f"{favorite_pizzas[0]}, {favorite_pizzas[1]}, {favorite_pizzas[2]}, I really love pizza!")
--- cap4/4_7.py
+++
@@ -1,3 +0,0 @@
-print("multiplos de tres...")
-for i in range(3,30,3):
-	print(i)
